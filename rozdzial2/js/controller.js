d3533658188e0e1ed023c661a6951b812c630e2e
Barbara Kopiec <bakopiec@gmail.com>
2018-12-18 21:05:50 +0100

    Learning about angular directives

--- rozdzial2/js/controller.js
+++ rozdzial2/js/controller.js
@@ -5,4 +5,28 @@ angular
     $scope.clickHandler = function() {
         $scope.isHidden = !$scope.isHidden;
     }
-});
+    $scope.contacts = [
+        {
+            name: 'Janko Walski',
+            phone: '01234567890', 
+            email: 'Janko@walski.com'
+        },
+        {
+            name: 'Karen Nerka',
+            phone: '0987654321',
+            email: 'karenn@email.com'
+        }
+    ];
+    $scope.center = false;
+    $scope.error = true;
+    $scope.styleDemo = function() {
+        if (!$scope.styler) {
+            return;
+        }
+        return {
+            background: 'red',
+            fontWeight: 'bold'
+        };
+    };
+    
+});         
